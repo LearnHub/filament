ebf4818eb0e615c5984bb0c38e414525b176db19
Rupert Rawnsley <rupert.rawnsley@gmail.com>
2021-12-01 08:19:16 +0000

    importShared now available in main library as importTexture

--- android/filament-android/src/main/cpp/Texture.cpp
+++ android/filament-android/src/main/cpp/Texture.cpp
@@ -100,14 +100,6 @@ Java_com_google_android_filament_Texture_nBuilderLevels(JNIEnv*, jclass,
     builder->levels((uint8_t) levels);
 }
 
-//AVN: Hack to allow existing texture to be used by filament
-extern "C" JNIEXPORT void JNICALL
-Java_com_google_android_filament_Texture_nBuilderImport(JNIEnv*, jclass,
-                                                        jlong nativeBuilder, jint id) {
-    Texture::Builder *builder = (Texture::Builder *) nativeBuilder;
-    builder->import((intptr_t) id);
-}
-
 extern "C" JNIEXPORT void JNICALL
 Java_com_google_android_filament_Texture_nBuilderSampler(JNIEnv*, jclass,
         jlong nativeBuilder, jint sampler) {
